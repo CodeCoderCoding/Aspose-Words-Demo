89d03e982f558344676a9c31dc374fa018b5385d
Neil <1845371176@qq.com>
2024-04-08 15:55:19 +0800

    fix comment parsing error

--- src/main/java/com/supremepole/aspose/words/comment/CommentParser.java
+++ src/main/java/com/supremepole/aspose/words/comment/CommentParser.java
@@ -26,15 +26,18 @@ public class CommentParser {
             // 获取注释范围内的文本内容
             StringBuilder sb = new StringBuilder();
             Node currentNode = comment.getPreviousSibling();
-            if (currentNode.getNodeType() == NodeType.RUN) {
-                Run run = (Run) currentNode;
-                sb.insert(0, run.getText());
-            } else if (currentNode.getNodeType() == NodeType.PARAGRAPH) {
-                Paragraph para = (Paragraph) currentNode;
-                for (int i = para.getRuns().getCount() - 1; i >= 0; i--) {
-                    Run run = para.getRuns().get(i);
+            while(currentNode.getNodeType()!=NodeType.COMMENT_RANGE_START){
+                if (currentNode.getNodeType() == NodeType.RUN) {
+                    Run run = (Run) currentNode;
                     sb.insert(0, run.getText());
+                } else if (currentNode.getNodeType() == NodeType.PARAGRAPH) {
+                    Paragraph para = (Paragraph) currentNode;
+                    for (int i = para.getRuns().getCount() - 1; i >= 0; i--) {
+                        Run run = para.getRuns().get(i);
+                        sb.insert(0, run.getText());
+                    }
                 }
+                currentNode=currentNode.getPreviousSibling();
             }
             System.out.println("Range Text: " + sb.toString());
 
